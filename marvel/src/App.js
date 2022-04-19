a4576e70e23f0a5d526406ced7dac8076d6a0eca
kubilay <kubidik27@gmail.com>
2022-04-19 15:45:15 +0300

    eslint installed and integrated

--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className='sada'>
+        </div>
+    );
 }
 
 export default App;
