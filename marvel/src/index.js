f1a04f1a6e4e40a3f1fc40a76f97ccdcdd1a189a
kubilay <kubidik27@gmail.com>
2022-04-19 15:52:06 +0300

    react router dom installed and integrated

--- marvel/src/index.js
+++ marvel/src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router} from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
-root.render(<App name="kubilay"/>);
+root.render(
+    <Router>
+        <App name="kubilay"/>
+    </Router>
+);
 
