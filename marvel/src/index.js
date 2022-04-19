dde3e9eb6d1b697ee8a2bf2fd9babbde3cda1890
kubilay <kubidik27@gmail.com>
2022-04-19 15:40:49 +0300

    file structure created/index.js edited based on React 18

--- marvel/src/index.js
+++ marvel/src/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+const container = document.getElementById("root");
+const root = ReactDOMClient.createRoot(container);
+root.render(<App name="kubilay"/>);
 
