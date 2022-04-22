6646bfd8fd53ffc561600ef24a6aa581b03ae99f
kubilay <kubidik27@gmail.com>
2022-04-23 01:18:15 +0300

    detailing from searchlist completed

--- marvel/src/components/SearchList.js
+++ marvel/src/components/SearchList.js
@@ -2,11 +2,14 @@ import React, { useContext, useState, useEffect, useRef }from "react";
 import style from "../styles/searchlist.module.scss";
 import CharacterContext from "../contexts/CharacterContext";
 import useOutsideClick from "../hooks/useOutsideClick";
+import { useNavigate } from "react-router-dom";
+
 
 function SearchList() {
-    const { searchList } = useContext(CharacterContext);
+    const { searchList,getDetail } = useContext(CharacterContext);
     const [list, setList] = useState([]);
     const listRef = useRef();
+    const navigate = useNavigate();
 
     useEffect(() => {
         setList(searchList.slice(0,5));
@@ -15,12 +18,18 @@ function SearchList() {
     useOutsideClick(listRef, () => {
         setList([]);
     });
+
+    const handleClick = (id) => {
+        getDetail(id);
+        navigate(`/detail/${id}`);
+    };
+
     return (
         <div className={style.searchList}>
             <div ref={listRef} className={style.list}>
                 {
                     list.map(item => (
-                        <span key={item.id}>
+                        <span onClick={()=>handleClick(item.id)} key={item.id}>
                         Karakter Adı : {item.name}
                         </span>
                     ))
