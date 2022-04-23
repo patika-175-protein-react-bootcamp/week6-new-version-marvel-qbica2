91b1ee76ac3bdb5f91df6e9687288589bad49608
kubilay <kubidik27@gmail.com>
2022-04-23 04:15:20 +0300

    searchlist responsive

--- marvel/src/components/Navigation.js
+++ marvel/src/components/Navigation.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import style from "../styles/navigation.module.scss";
 import SearchBar from "./SearchBar";
 import LanguageSelection from "./LanguageSelection";
+import useWindowSize from "../hooks/useWindowSize";
 
 function Navigation() {
+    const [width, ] = useWindowSize();
+    const [bigScreen, setBigScreen] = useState(true);
+
+    useEffect(() => {
+        if (width > 950) {
+            setBigScreen(true);
+        } else {
+            setBigScreen(false);
+        }
+    }, [width]);
+    
     return (
         <div className={style.nav}>
-            <SearchBar />
-            <LanguageSelection />
+            {
+                bigScreen ? (<>
+                    <SearchBar />
+                    <LanguageSelection /> 
+                </>) : (<>
+                    <LanguageSelection />
+                    <SearchBar />
+                </>)
+            }
+            
         </div>
     );
 }
